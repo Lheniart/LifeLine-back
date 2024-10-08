89294a4b44b66b1fd8720949202ba98f5130434f
LoanH <lheniart@be-infotech.fr>
2024-10-08 14:51:58 +0200

    chore(backend): setup project

---
+++ src/main/java/fr/epsi/lifelineback/DAE/DaeController.java
@@ -0,0 +1,10 @@
+package fr.epsi.lifelineback.DAE;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DaeController {
+
+}
---
+++ src/main/java/fr/epsi/lifelineback/LifelineBackApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.lifelineback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifelineBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifelineBackApplication.class, args);
+    }
+
+}
